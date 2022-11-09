fb8dbfef7cda8eea61a7ea9346592f6d8dc47d4d
yeyuanhua <1378476425@qq.com>
2022-11-09 15:59:58 +0800

    添加搜索

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Yvonne",
-  tagline: "",
+  tagline: ``,
   url: "https://yyh28.top",
   baseUrl: "/",
   onBrokenLinks: "throw",
@@ -89,6 +89,28 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        // Algolia 提供的应用 ID
+        appId: "N57U6EED72",
+
+        //  公开 API 密钥：提交它没有危险
+        apiKey: "11a64105cfc3b5ee3edeea2893fff3ce",
+
+        indexName: "yvonne",
+        // 可选：见下文
+        contextualSearch: true,
+
+        // 可选：声明哪些域名需要用 window.location 型的导航而不是 history.push。 适用于 Algolia 配置会爬取多个文档站点，而我们想要用 window.location.href 在它们之间跳转时。
+        externalUrlRegex: "external\\.com|domain\\.com",
+
+        // 可选：Algolia 搜索参数
+        searchParameters: {},
+
+        // 可选：搜索页面的路径，默认启用（可以用 `false` 禁用）
+        searchPagePath: "search",
+
+        // ……其他 Algolia 参数
       },
     }),
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import yvonne from "/static/img/yvonne.png"
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import yvonne from "/static/img/yvonne.png";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  console.log("%c siteConfig", "font-size:13px; background:pink; color:#bf2c9f;", siteConfig);
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">
-          <img src={yvonne} alt="" /></h1>
+        <h1 className={styles["hero__title"]}>
+          <img src={yvonne} alt="" />
+        </h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">前端：</p>
+        <p className="hero__subtitle">后端：</p>
+        <p className="hero__subtitle">运维：docker、git</p>
+        <p className="hero__subtitle">其他：信息系统项目管理师、mermaid使用、markmap使用、docusaurus使用</p>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
     </Layout>
   );
