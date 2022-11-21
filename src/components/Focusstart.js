60b244b612aaabd92c54f88885869d96eff45d6c
yeyuanhua <1378476425@qq.com>
2022-11-21 15:13:46 +0800

    补充

---
+++ src/components/Focusstart.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Highlight from "./Highlight";
+export default () => {
+  return (
+    <span
+      style={{
+        backgroundColor: color,
+        borderRadius: "2px",
+        color: "#fe0808",
+        padding: "0.2rem",
+      }}
+    >
+      ★
+    </span>
+  );
+};
