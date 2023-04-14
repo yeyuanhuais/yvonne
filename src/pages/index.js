c2db213419884ff582e59cc9e2383dad9ae169f2
yeyuanhua <1378476425@qq.com>
2023-04-14 14:27:02 +0800

    git

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,28 +8,33 @@ import yvonne from "/static/img/yvonne.png";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className={styles["hero__title"]}>
-          <img src={yvonne} alt="" />
-        </h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <p className="hero__subtitle">前端：</p>
-        <p className="hero__subtitle">后端：</p>
-        <p className="hero__subtitle">运维：docker、git</p>
-        <p className="hero__subtitle">其他：信息系统项目管理师知识点总结、mermaid使用、markmap使用、docusaurus使用</p>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx("hero hero--primary", styles.heroBanner)}>
+            <div className="container">
+                <h1 className={styles["hero__title"]}>
+                    <img src={yvonne} alt="" />
+                </h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <p className="hero__subtitle">前端：js</p>
+                <p className="hero__subtitle">后端：</p>
+                <p className="hero__subtitle">运维：docker、git、nginx</p>
+                <p className="hero__subtitle">
+                    其他：信息系统项目管理师知识点总结、mermaid使用、markmap使用、docusaurus使用
+                </p>
+            </div>
+        </header>
+    );
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-    </Layout>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <Layout
+            title="学习记录"
+            description="Description will go into a meta tag in <head />"
+        >
+            <HomepageHeader />
+        </Layout>
+    );
 }
