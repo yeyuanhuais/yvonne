ab2899cdf0462ad02c65a6eaa143e8e1a2e61457
yeyuanhua <1378476425@qq.com>
2023-07-26 14:11:50 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,10 +141,10 @@ const config = {
             },
             algolia: {
                 // Algolia 提供的应用 ID
-                appId: "N57U6EED72",
+                appId: "B42DC87VTC",
                 //  公开 API 密钥：提交它没有危险
-                apiKey: "11a64105cfc3b5ee3edeea2893fff3ce",
-                indexName: "yvonne",
+                apiKey: "bee5394a2b2f9e634ff84fcd179d78f3",
+                indexName: "yyh28.top",
                 // 可选：见下文
                 contextualSearch: true,
                 // 可选：声明哪些域名需要用 window.location 型的导航而不是 history.push。 适用于 Algolia 配置会爬取多个文档站点，而我们想要用 window.location.href 在它们之间跳转时。
